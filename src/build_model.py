635603f2be73cce5aa0fc4978796c085242cfa3c
steven <porn52hub78@gmail.com>
2021-02-01 17:17:10 +0800

    finish model impl

--- src/build_model.py
+++ src/build_model.py
@@ -1,11 +1,49 @@
 import itertools
 import os
 import time
-
 from datetime import datetime
-
 import numpy as np
 import torch
 import torchvision.utils as vutils
-
 import utils
+from cgan import (Generator, Discriminator)
+
+class ConditionModel(object):
+    def __init__(
+        self,
+        name,
+        device,
+        data_loader,
+        classes,
+        channels,
+        img_size,
+        latent_dim
+    ):
+    self.name = name
+    self.device = device
+    self.data_loader = data_loader
+    self.classes = classes
+    self.channels = channels
+    self.img_size = img_size
+    self.latent_dim = latent_dim
+    self.NetG = None
+    self.NetD = None
+    self.optim_G = None
+    self.optim_D = None
+    if self.name == 'cgan':
+        self.NetG = Generator(
+            self.classes, 
+            self.channels, 
+            self.img_size, 
+            self.latent_dim
+        )
+        self.NetD = Discriminator(
+            self.classes, 
+            self.channels, 
+            self.img_size, 
+            self.latent_dim
+        )
+        self.NetG.to(self.device)
+        self.NetD.to(self.device)
+    assert (self.NetG != None and self.NetD != None) \
+                    f'Both generator and discriminator cant be none' 
--- src/cgan.py
+++ src/cgan.py
@@ -31,7 +31,7 @@ class Generator(nn.Module):
             nn.Tanh()
         )
 
-    self._create_linear_layer(self, input_size, output_size, normalize = True):
+    def _create_linear_layer(self, input_size, output_size, normalize = True):
         layers = [nn.linear(input_size, output_size)]
         if normalize:
             layers.append(nn.BatchNorm1d(output_size))
@@ -46,3 +46,77 @@ class Generator(nn.Module):
         x = self.model(z)
         x = x.view(x.size(0), *self.img_shape) # x.shape = [batch_size, *image_shape]
         return x
+
+class Discriminator(nn.Module):
+    def __init__(self, classes, channels, img_size, latent_dim):
+        super(Discriminator, self).__init__()
+        self.classes = classes
+        self.channels = channels
+        self.img_size = img_size
+        self.latent_dim = latent_dim
+        self.img_shape = (
+            self.channels, 
+            self.img_size, 
+            self.img_size
+        )
+        self.label_embedding = nn.Embedding(self.classes, self.classes)
+        self.adversarial_loss = torch.nn.BCELoss()
+        '''
+        The discriminator network:
+        - consists of 5 linear layers
+        - 2 of which are connected to Dropout layer(for enhance generlization capability)
+        - In the last, using a sigmoid function to make sure the output value is lie within the range [0,1]
+        '''
+        self.model = nn.Sequential(
+            *self._create_linear_layer(
+                input_size = self.classes + int(torch.prod(self.img_shape))
+                output_size = 1024,
+                drop_out = False,
+                activation_func = True
+            ),
+            *self._create_linear_layer(
+                input_size = 1024
+                output_size = 512,
+                drop_out = True,
+                activation_func = True
+            ),
+            *self._create_linear_layer(
+                input_size = 512
+                output_size = 256,
+                drop_out = True,
+                activation_func = True
+            ),
+            *self._create_linear_layer(
+                input_size = 256
+                output_size = 128,
+                drop_out = False,
+                activation_func = False
+            ),
+            *self._create_linear_layer(
+                input_size = 128
+                output_size = 1,
+                drop_out = False,
+                activation_func = False
+            ),
+            nn.Sigmoid()
+        )
+    def _create_linear_layer(self, input_size, output_size, drop_out = True, activation_func = True):
+        layers = [nn.linear(input_size, output_size)]
+        if drop_out:
+            layers.append(nn.Dropout(0.4))
+        if activation_func:
+            layers.append(nn.LeakyReLU(0.2, inplace = True))
+        return layers
+
+    def forward(self, output, label):
+        '''
+        include the label information in input by concatenation the embedding of labels with image vector
+        '''
+        x = torch.cat(
+            (image.view(image.size(0), -1), self.label_embedding(labels)),
+            -1
+        )
+        return self.model(x)
+
+    def loss(self, output, label):
+        return self.adversarial_loss(output, label)
